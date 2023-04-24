f0d5971fc178cbfcb3750dfc76029eab79c76747
Salman <therodfighter@gmail.com>
2023-04-25 03:35:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const burgermenu = document.querySelector("#burger-menu")
+const dropNavbar = document.getElementsByClassName(".drop-navbar")
+
+burgermenu.addEventListener("click", () => {
+    alert("hello")
+})
